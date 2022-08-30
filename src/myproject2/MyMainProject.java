f1d0a04ceda7c995b03d4a796f421d544763bcf1
Ravin14 <rahmatalvin@student.uns.ac.id>
2022-08-30 13:37:29 +0700

    first commit

---
+++ src/myproject2/MyMainProject.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MyMainProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("     A        V         V  N       N");
+        System.out.println("    A A        V       V   N N     N");
+        System.out.println("   A   A        V     V    N   N   N");
+        System.out.println("  A A A A        V   V     N    N  N");
+        System.out.println(" A       A        V V      N     N N");
+        System.out.println("A         A        V       N       N");
+        // TODO code application logic here
+    }
+    
+}
