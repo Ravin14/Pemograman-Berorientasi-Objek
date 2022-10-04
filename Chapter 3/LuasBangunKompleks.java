8e4a947c18e9d4d78c0d3393e0618b8f7ff1b08c
Ravin14 <89761323+Ravin14@users.noreply.github.com>
2022-10-04 15:08:09 +0700

    Add files via upload

---
+++ Chapter 3/LuasBangunKompleks.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LuasBangunKompleks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+     * @param args the command line arguments
+     */
+        //Project 1
+        
+        // hitung luas setiap ruang
+        
+        Lingkaran A = new Lingkaran();
+        A.r = 21;
+        
+        Persegi B = new Persegi();
+        B.s = 42;
+        
+        // hitung luas total bidang dalam m2
+        double totalLuasProject1 = (2 * A.hitungLuas()) + B.hitungLuas();
+        System.out.println("Total luas bangun datar tersebut adalah " + totalLuasProject1 + "cm²");
+        
+        
+        // Project 2
+        
+        // hitung luas setiap ruang
+        
+        // setengah lingkaran besar
+        Lingkaran C = new Lingkaran();
+        C.r = 14;
+        
+        // 1 lingkaran kecil
+        Lingkaran D = new Lingkaran();
+        D.r = 7;
+        
+        // hitung luas total bidang dalam m2
+        double totalLuasProject2 = (0.5 * C.hitungLuas()) -  D.hitungLuas();
+        System.out.println("Total luas bangun datar tersebut adalah " + totalLuasProject2+ "cm²");
+    }
+    
+        // TODO code application logic here
+    }
+    
